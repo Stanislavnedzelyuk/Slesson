4cc61b6d09538f52d2641c30c84cb17e1e301068
Stanislav <101831853+Stanislavnedzelyuk@users.noreply.github.com>
2023-06-13 10:34:12 +0400

    Initial commit

---
+++ src/test/java/Slesson/Slesson/SlessonApplicationTests.java
@@ -0,0 +1,13 @@
+package Slesson.Slesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlessonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
