37f8089be882acdd660704b6037a196325b322b6
Stanislav <101831853+Stanislavnedzelyuk@users.noreply.github.com>
2023-06-13 10:34:37 +0400

    Домашняя работа

---
+++ src/main/java/Slesson/Slesson/ShoppingCartController.java
@@ -0,0 +1,38 @@
+package Slesson.Slesson;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/store/order")
+public class ShoppingCartController {
+
+    @PostMapping("/add")
+    public void addToCart(@RequestBody List<Integer> itemIds, HttpSession session) {
+        List<Integer> cart = getCartInSession(session);
+        cart.addAll(itemIds);
+    }
+
+    @GetMapping("/get")
+    public List<Integer> getCart(HttpSession session) {
+        return getCartInSession(session);
+    }
+
+    @SuppressWarnings("unchecked")
+    private List<Integer> getCartInSession(HttpSession session) {
+        List<Integer> cart = (List<Integer>) session.getAttribute("CART_KEY");
+        if (cart == null) {
+            cart = new ArrayList<>();
+            session.setAttribute("CART_KEY", cart);
+        }
+        return cart;
+    }
+}
+
+
---
+++ src/main/java/Slesson/Slesson/SlessonApplication.java
@@ -0,0 +1,13 @@
+package Slesson.Slesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlessonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlessonApplication.class, args);
+	}
+
+}
