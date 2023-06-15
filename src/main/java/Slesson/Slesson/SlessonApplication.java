78c7fe6db3fc836422d6576f7ed314a6442e5dd9
Stanislav <101831853+Stanislavnedzelyuk@users.noreply.github.com>
2023-06-15 08:41:28 +0300

    Домашняя работа с уточнениями

--- src/main/java/Slesson/Slesson/SlessonApplication.java
+++ src/main/java/Slesson/Slesson/SlessonApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SlessonApplication {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(SlessonApplication.class, args);
 
